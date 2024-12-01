84268f108cf3f20c4e75b4754eca54c1a1b6d645
Taqdees Mehdi <taqdeesm99@gmail.com>
2024-12-01 13:55:21 +0500

    fixed

--- main.js
+++ main.js
@@ -1,43 +1,20 @@
-// const navToggle = document.querySelector("primary-nav");
-
-// const navToggle = document.querySelector('[aria-controls="primary-nav"]');
-// const primarynav = document.querySelector("#primary-nav");
-// navToggle.addEventListener("click", () => {
-//   const navopened = navToggle.getAttribute("aria-expanded");
-//   if (navopened === "false") {
-//     navToggle.setAttribute("aria-expanded", "true");
-//   } else {
-//     navToggle.setAttribute("aria-expanded", "false");
-//   }
-// });
-
-// const hamburgerButton = document.querySelector(
-//   '[aria-controls="primary-navigation"]'
-// );
-// const nav = document.querySelector(".primary-navigation");
-
-// hamburgerButton.addEventListener("click", () => {
-//   // check if the navigation is opened
-//   const isNavOpened = hamburgerButton.getAttribute("aria-expanded");
-
-//   if (isNavOpened === "false") {
-//     hamburgerButton.setAttribute("aria-expanded", "true");
-//   } else {
-//     hamburgerButton.setAttribute("aria-expanded", "false");
-//   }
-// });
-
 document.addEventListener("DOMContentLoaded", () => {
-  const navToggle = document.querySelector('[aria-controls="primary-nav"]');
-  const primarynav = document.querySelector("#primary-nav");
+  const hamburgerButton = document.querySelector(
+    '[aria-controls="primary-nav"]'
+  );
+  const nav = document.querySelector(".primary-nav");
 
-  navToggle.addEventListener("click", () => {
-    const navopened = navToggle.getAttribute("aria-expanded");
-    if (navopened === "false") {
-      navToggle.setAttribute("aria-expanded", "true");
-    } else {
-      navToggle.setAttribute("aria-expanded", "false");
-    }
-    console.log(navopened);
-  });
+  if (hamburgerButton) {
+    hamburgerButton.addEventListener("click", () => {
+      const isNavOpened = hamburgerButton.getAttribute("aria-expanded");
+
+      if (isNavOpened === "false") {
+        hamburgerButton.setAttribute("aria-expanded", "true");
+      } else {
+        hamburgerButton.setAttribute("aria-expanded", "false");
+      }
+    });
+  } else {
+    console.error("Hamburger button not found");
+  }
 });
